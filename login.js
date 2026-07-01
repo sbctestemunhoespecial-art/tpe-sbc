0da085397b483bfe16b31007742f0775b55211ac
uezleimatos <uezleimatos52@gmail.com>
2026-07-01 20:36:19 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwrlEvENxytMFmrTmzSWDmXCXcy-0dBU7ve5fWRVf871plhTW5TqvtsS4-9LiwjnXvU/exec";
 
-/*function fazerLogin() {
+function fazerLogin() {
 
   const email = document.getElementById('emailLogin').value.trim();
   const senha = document.getElementById('senhaLogin').value.trim();
@@ -39,78 +39,4 @@ const API_URL = "https://script.google.com/macros/s/AKfycbwrlEvENxytMFmrTmzSWDmX
     "&callback=" + callback;
 
   document.body.appendChild(script);
-}*/
-
-function fazerLogin() {
-  const email = document.getElementById('emailLogin').value.trim();
-  const senha = document.getElementById('senhaLogin').value.trim();
-  const msgErro = document.getElementById('msgLogin');
-
-  msgErro.textContent = '';
-  mostrarSpinner();
-
-  fetch(API_URL + `?acao=login&email=${encodeURIComponent(email)}&senha=${encodeURIComponent(senha)}`)
-    .then(r => r.json())
-    .then(res => {
-
-      if (res && res.perfil) {
-
-        perfilUsuario = res.perfil;
-        idUsuarioLogado = res.id;
-
-        const saudacaoEl = document.getElementById("saudacaoUsuario");
-        const tipoAcessoEl = document.getElementById("tipoAcessoUsuario");
-
-        if (saudacaoEl) {
-            saudacaoEl.textContent = "Bem-vindo(a) 👋";
-        }
-
-        if (tipoAcessoEl) {
-            tipoAcessoEl.textContent = `Seu tipo de acesso é ${perfilUsuario}`;
-        }
-
-        document.getElementById('emailLogin').value = '';
-        document.getElementById('senhaLogin').value = '';
-        msgErro.textContent = '';
-
-        localStorage.setItem("usuarioLogado", JSON.stringify({
-          id: res.id,
-          perfil: res.perfil,
-          timestamp: Date.now()
-        }));
-
-        document.getElementById('telaLogin').style.display = 'none';
-        document.getElementById('conteudoProtegido').style.display = 'block';
-
-        historico.length = 0;
-
-        document.querySelectorAll('.tela').forEach(el => {
-            el.classList.remove('aberta');
-        });
-
-        document.getElementById('menuCards')
-            ?.classList.add('aberta');
-
-        telaAtual = 'menuCards';
-
-        atualizarBotaoVoltar();
-
-        mostrarSecoesPorPerfil(res.perfil);
-
-        limparCamposUsuario();
-        restaurarCamposPerfil();
-
-        esconderSpinner();
-
-        verificarTreinamentoPendente();
-
-      } else {
-        esconderSpinner();
-        msgErro.textContent = res.mensagem || '❌ Erro desconhecido ao fazer login.';
-      }
-    })
-    .catch(err => {
-      esconderSpinner();
-      msgErro.textContent = '❌ Erro ao fazer login: ' + err.message;
-    });
 }

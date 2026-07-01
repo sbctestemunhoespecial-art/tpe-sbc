da8123a44557feaede712eb5e680faf5c1fb011e
uezleimatos <uezleimatos52@gmail.com>
2026-07-01 10:35:33 -0300

    Fazer login alterado
    Fazer login alterado

--- login.js
+++ login.js
@@ -1,45 +1,40 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwrlEvENxytMFmrTmzSWDmXCXcy-0dBU7ve5fWRVf871plhTW5TqvtsS4-9LiwjnXvU/exec";
 
-async function fazerLogin() {
+function fazerLogin() {
 
   const email = document.getElementById('emailLogin').value.trim();
   const senha = document.getElementById('senhaLogin').value.trim();
-
-console.log("INPUT EMAIL:", document.getElementById('emailLogin'));
-console.log("INPUT SENHA:", document.getElementById('senhaLogin'));
-
-console.log("EMAIL VALUE:", document.getElementById('emailLogin').value);
-console.log("SENHA VALUE:", document.getElementById('senhaLogin').value);
-
   const msgErro = document.getElementById('msgLogin');
 
   msgErro.textContent = '';
   mostrarSpinner();
 
-  try {
+  const script = document.createElement("script");
 
-    const url = `${API_URL}?acao=login&email=${encodeURIComponent(email)}&senha=${encodeURIComponent(senha)}`;
+  const callbackName = "loginCallback_" + Date.now();
 
-    const response = await fetch(url);
-    const res = await response.json();
+  window[callbackName] = function(res) {
 
     esconderSpinner();
 
     if (res.sucesso) {
-
       alert("Login OK");
-
       document.getElementById("telaLogin").style.display = "none";
       document.getElementById("conteudoProtegido").style.display = "block";
-
     } else {
       msgErro.textContent = res.mensagem;
     }
 
-  } catch (err) {
+    delete window[callbackName];
+    document.body.removeChild(script);
+  };
 
-    esconderSpinner();
-    msgErro.textContent = "Erro: " + err.message;
+  script.src =
+    API_URL +
+    "?acao=login" +
+    "&email=" + encodeURIComponent(email) +
+    "&senha=" + encodeURIComponent(senha) +
+    "&callback=" + callbackName;
 
-  }
+  document.body.appendChild(script);
 }
